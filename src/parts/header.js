d732e833d9ff42b33542402d750af41650963ac2
Mr.RySes <mpoprev2@email.com>
2023-04-13 04:56:17 +0800

    04-13-23 04:58

---
+++ src/parts/header.js
@@ -0,0 +1,25 @@
+export default function header(){
+	const __exclusive__ = {
+		backgroundColor: "#131320",
+		color: "#ffffff",
+		textAlign: "left",
+		padding: "0 0.25em "
+	}
+	const noPadd = {
+		margin: "0",
+		padding: "0"
+	}
+	const lPad = {
+		margin: "0",
+		padding: "0",
+		paddingLeft: "0.50em"
+	}
+	return(
+		<div className="App-Header" style={__exclusive__}>
+			<header>
+				<h3 style={noPadd}>Title Name</h3>
+				<h5 style={lPad}>Developed by MPOP Reverse II</h5>
+			</header>
+		</div>
+	)
+}
