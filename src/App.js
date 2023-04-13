eefce6dfef756c21f2c8b3d56959a2974b86b471
Mr.RySes <mpoprev2@email.com>
2023-04-13 12:12:11 +0800

    04-13-23 12:12

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from "./parts/header"
+import Footer from './parts/footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/parts/footer.js
@@ -0,0 +1,11 @@
+export default function Footer(){
+	const bg = {
+		backgroundColor: "#131320",
+		color: "#ffffff"
+	}
+	return(
+		<div style={bg} className="Footer">
+			<h3 className="text-center">Developed by RyannKim327</h3>
+		</div>
+	)
+}
